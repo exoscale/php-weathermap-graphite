5c02ce4e3ca1ebf7b26f33c0508c5ccc56923923
XioNoX <xionox@gmail.com>
2012-11-15 17:27:09 +0100

    Update WeatherMapDataSource_graphite.php
    Adding details on how to use it

--- WeatherMapDataSource_graphite.php
+++ WeatherMapDataSource_graphite.php
@@ -1,10 +1,18 @@
 <?php
 // Datasource for Graphite (http://graphite.wikidot.com/)
-// - currently reports same value for in and out (used with LINKSTYLE onway)
+// Quick and dirty script based on https://github.com/alexforrow/php-weathermap-graphite
+// Arzhel Younsi
 
-// TARGET graphite:graphite_url/metric
-//      e.g. graphite:system.example.com:8081/devices.servers.XXXXX.system.load.1min
-
+// TARGET graphite:(graphite_server)|in|(out)|(type)
+//      e.g. graphite:graphite.example.net:82|network.net1.router1_example_net.int.xe_0_0_2.outbytes|network.net1.router1_example_net.int.xe_0_0_2.inbytes|interface
+//
+//  host - graphite server to use, if empty it will use the value define below to keep the weathermap conf clean
+//  in   - string of the ressource to graph
+//  out  - (optionnal) string of the ressource to graph for the other direction
+//  type - (optionnal) if set to "interface" will add "scale(scaleToSeconds(nonNegativeDerivative($targetIn),1),8)"
+//         to get the numbers as bytes per seconds. (Doesn't work yet if the counter wraps)
+//         You can also add the operators directly to the in or out value.
+//
 class WeatherMapDataSource_graphite extends WeatherMapDataSource {
 
     private $regex_pattern = "/^graphite:([^|]*)|([^|]+)|([^|]*)|([^|]*)$/";
